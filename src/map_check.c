bd2f92d03c73daf900172863df29f09c29092b7d
ilyasslefhail <ilyasslefhaili15@gmail.com>
2022-08-25 11:59:46 +0100

    norme is loading

--- src/map_check.c
+++ src/map_check.c
@@ -12,25 +12,3 @@
 
 #include "../cub.h"
 
-int	check_map_borders(t_map_data map_data)
-{
-	int		i;
-	int		j;
-	char	*temp;
-
-	i = 0;
-	j = 0;
-	while (map_data.map[i])
-	{
-		j = 0;
-		temp = ft_strtrim(map_data.map[i], "\t ");
-		if (temp[j] != '1')
-			ft_error(1, "Map must be surrounded by walls\n");
-		while (map_data.map[i][j])
-		{
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
